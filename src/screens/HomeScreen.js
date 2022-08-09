e016f565172abb8883f3a28156a35468eb5a5b4d
Celdama <celdama.dev@gmail.com>
2022-08-09 08:23:57 +0200

    Create HomeScreen

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,9 @@
+import { StyleSheet, Text } from 'react-native';
+
+const HomeScreen = () => {
+  return <Text>Home</Text>;
+};
+
+const styles = StyleSheet.create({});
+
+export default HomeScreen;
