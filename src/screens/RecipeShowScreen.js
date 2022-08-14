787d799d4135b440cc01281b48f5bef2a8b334dd
Celdama <celdama.dev@gmail.com>
2022-08-14 18:21:19 +0200

    screens/RecipeShowScreen: display Recipe's image

--- src/screens/RecipeShowScreen.js
+++ src/screens/RecipeShowScreen.js
@@ -1,26 +1,49 @@
-import { useEffect } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { Image, StyleSheet, Text, View } from 'react-native';
 
 import spoonacular from '../api/spoonacular';
 
 const RecipeShowScreen = ({ navigation }) => {
   const id = navigation.getParam('id');
+  const [recipe, setRecipe] = useState(null);
 
   const getResult = async (id) => {
-    const response = await spoonacular.get(`/${id}/information`);
-    console.log(response.data);
+    try {
+      const response = await spoonacular.get(`/${id}/information`);
+      setRecipe(response.data);
+    } catch (err) {
+      console.log('Something went wrong, reload App');
+    }
   };
 
   useEffect(() => {
     getResult(id);
   }, []);
   return (
-    <View>
+    <View style={styles.container}>
+      {recipe ? (
+        <Image
+          style={styles.image}
+          source={{
+            uri: `https://spoonacular.com/recipeImages/${recipe.id}-636x393.${recipe.imageType}`,
+          }}
+        />
+      ) : null}
       <Text>{id}</Text>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    borderWidth: 1,
+    borderColor: 'red',
+    // flex: 1,
+  },
+  image: {
+    width: 200,
+    height: 200,
+  },
+});
 
 export default RecipeShowScreen;
