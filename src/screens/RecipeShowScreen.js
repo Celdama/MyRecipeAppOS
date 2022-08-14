e6b7a983b9f4f810d988f7d120b11cb28ad67739
Celdama <celdama.dev@gmail.com>
2022-08-14 09:31:14 +0200

    screens/RecipeShowScreen: create

---
+++ src/screens/RecipeShowScreen.js
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+const RecipeShowScreen = () => {
+  return (
+    <View>
+      <Text>Recipe Show Screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default RecipeShowScreen;
