1ccca8552191e8f00d6b586daffe4c6540b14161
Celdama <celdama.dev@gmail.com>
2022-08-14 09:01:40 +0200

    components/RecipesList: render recipes List in RecipesList component using FlatList done

--- src/components/RecipesList.js
+++ src/components/RecipesList.js
@@ -1,10 +1,17 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
 
 const RecipesList = ({ title, recipes }) => {
   return (
     <View>
       <Text>Recipes List</Text>
       <Text>{recipes.length}</Text>
+      <FlatList
+        data={recipes}
+        keyExtractor={(recipe) => recipe.id}
+        renderItem={({ item }) => {
+          return <Text>{item.title}</Text>;
+        }}
+      />
     </View>
   );
 };
